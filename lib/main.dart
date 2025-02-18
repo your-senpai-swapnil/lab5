6245fb36513d1c4ebaf6acbbbc338590bd96ddfc
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-02-18 11:22:48 +0600

    overflow_task

--- lib/main.dart
+++ lib/main.dart
@@ -17,38 +17,41 @@ class LabClass05 extends StatelessWidget {
               backgroundColor: const Color.fromARGB(155, 36, 2, 254),
               elevation: 23,
             ),
-            body: Container(
-              height: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: const Color.fromARGB(255, 201, 16, 176),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: const Color.fromARGB(255, 223, 239, 4),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: const Color.fromARGB(255, 30, 12, 226),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: const Color.fromARGB(255, 6, 240, 34),
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: const Color.fromARGB(255, 233, 15, 15),
-                  ),
-                ],
+            body: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                height: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.star,
+                      size: 50,
+                      color: const Color.fromARGB(255, 201, 16, 176),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 50,
+                      color: const Color.fromARGB(255, 223, 239, 4),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 50,
+                      color: const Color.fromARGB(255, 30, 12, 226),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 50,
+                      color: const Color.fromARGB(255, 6, 240, 34),
+                    ),
+                    Icon(
+                      Icons.star,
+                      size: 50,
+                      color: const Color.fromARGB(255, 233, 15, 15),
+                    ),
+                  ],
+                ),
               ),
             )));
   }
