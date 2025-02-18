ac9d20312779c287ede74ceeb13eaeef1e474e73
Swapnil <110291417+your-senpai-swapnil@users.noreply.github.com>
2025-02-18 15:11:25 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,110 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const LabClass05());
+  runApp(const MyApp());
 }
 
-class LabClass05 extends StatelessWidget {
-  const LabClass05({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Basic Flutter UI - 02",
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Basic Flutter UI - 2"),
-              centerTitle: true,
-              backgroundColor: const Color.fromARGB(155, 36, 2, 254),
-              elevation: 23,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xFFE3E8FF),
+        body: Center(
+          child: PriceCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class PriceCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 400,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // Discount Tag
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "50% OFF",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  Icons.favorite_border,
+                  color: Colors.grey,
+                  size: 24,
+                ),
+              ],
             ),
-            body: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Container(
-                height: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.star,
-                      size: 50,
-                      color: const Color.fromARGB(255, 201, 16, 176),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 50,
-                      color: const Color.fromARGB(255, 223, 239, 4),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 50,
-                      color: const Color.fromARGB(255, 30, 12, 226),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 50,
-                      color: const Color.fromARGB(255, 6, 240, 34),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 50,
-                      color: const Color.fromARGB(255, 233, 15, 15),
-                    ),
-                  ],
+          ),
+          
+          // Mi Band Image
+          Container(
+            width: 150,
+            height: 150,
+            child: CustomPaint(
+            ),
+          ),
+          
+          // Product Name
+          const Text(
+            "Nike Air Shoes",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          
+          // Price Section
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "450",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            )));
+              const SizedBox(width: 15),
+              Text(
+                "600",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey[500],
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
