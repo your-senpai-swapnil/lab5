b863f0f9d14673517f0eb2305f8739ffa4ea9773
your-senpai-swapnil <swapnil15-3477@diu.edu.bd>
2025-02-18 10:54:06 +0600

    axisalignment_task

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const LabClass05());
 }
 
-class LabClass05 extends StatelessWidget{
+class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title : "Basic Flutter UI - 02",
-      home: Scaffold(
-        appBar: AppBar(
-          
-          title: Text("Basic Flutter UI - 2"),
-          centerTitle: true,
-          backgroundColor:  const Color.fromARGB(155, 36, 2, 254),
-
-          elevation: 23,
-        ),
-        body: Row(
-          children: [
-            Icon(
-              Icons.star,
-              size: 50,
-              color: Colors.grey,
+        title: "Basic Flutter UI - 02",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Basic Flutter UI - 2"),
+              centerTitle: true,
+              backgroundColor: const Color.fromARGB(155, 36, 2, 254),
+              elevation: 23,
             ),
-            Icon(
-              Icons.star,
-              size: 50,
-              color: Colors.grey,
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-              color: Colors.grey,
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-              color: Colors.grey,
-            ),
-            Icon(
-              Icons.star_border,
-              size: 50,
-              color: Colors.grey,
-            ),
-          
-          ],
-        )
-      )
-    );
+            body: Container(
+              height: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.star,
+                    size: 50,
+                    color: const Color.fromARGB(255, 201, 16, 176),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 50,
+                    color: const Color.fromARGB(255, 223, 239, 4),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 50,
+                    color: const Color.fromARGB(255, 30, 12, 226),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 50,
+                    color: const Color.fromARGB(255, 6, 240, 34),
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 50,
+                    color: const Color.fromARGB(255, 233, 15, 15),
+                  ),
+                ],
+              ),
+            )));
   }
 }
